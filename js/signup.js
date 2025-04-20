a3d15d9d3cd02dddf075ad25a06409bfe27f1189
coreFront Technologies <sayyidyahaya888@gmail.com>
2025-04-21 00:58:31 +0100

    the project was completed successfully

--- js/signup.js
+++ js/signup.js
@@ -3,21 +3,17 @@ const email = document.getElementById("email");
 const err = document.getElementById("err");
 const err_r = document.getElementById("error");
 
-
 form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    const err_rValue = email.value;
-    const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
- 
+    e.preventDefault();
 
-    if (!emaiPattern.test(err_rValue)) {
+    const emailValue = email.value.trim();
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (!emailPattern.test(emailValue)) {
         email.classList.add('error');
         err.style.display = 'inline';
     } else {
-        window.location.href = "success.html"
-
+        localStorage.setItem('userEmail', emailValue);
+        window.location.href = "success.html";
     }
-
-
-})
-
+});
